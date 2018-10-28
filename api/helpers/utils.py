8c6212d7a3bc4b9056624421812bbd7dc0e68a3e
emischuk <emishuk@mmtr.ru>
2018-10-28 20:45:57 +0200

    refactoring

--- api/helpers/utils.py
+++ api/helpers/utils.py
@@ -4,52 +4,78 @@ import config
 from api.poker.state import State
 
 
+def get_array_for_cards_from_map(m=None):
+    return get_array_from_stages_for_cards(
+        m.get('pf0'), m.get('pf1'), m.get('f0'), m.get('f1'), m.get('f2'), m.get('t'), m.get('r')
+    )
+
+
 def get_array_inputs_cards(player=None):
+    return get_array_from_stages_for_cards(
+        player.hand[0], player.hand[1], player.table[0], player.table[1], player.table[2], player.table[3],
+        player.table[4]
+    )
+
+
+def get_array_from_stages_for_cards(pf0, pf1, f0, f1, f2, t, r):
     temp = [0] * 68
 
-    if len(player.hand) > 1:
-        temp[State.RANKS.index(player.hand[0].rank)] = 1
-        temp[State.SUITS.index(player.hand[0].suit) + 13] = 1
-        temp[State.RANKS.index(player.hand[1].rank)] = 1
-        temp[State.SUITS.index(player.hand[1].suit) + 13] = 1
-    if len(player.table) > 2:
-        temp[State.RANKS.index(player.table[0].rank) + 17] = 1
-        temp[State.SUITS.index(player.table[0].suit) + 30] = 1
-        temp[State.RANKS.index(player.table[1].rank) + 17] = 1
-        temp[State.SUITS.index(player.table[1].suit) + 30] = 1
-        temp[State.RANKS.index(player.table[2].rank) + 17] = 1
-        temp[State.SUITS.index(player.table[2].suit) + 30] = 1
-    if len(player.table) > 3:
-        temp[State.RANKS.index(player.table[3].rank) + 34] = 1
-        temp[State.SUITS.index(player.table[3].suit) + 47] = 1
-    if len(player.table) > 4:
-        temp[State.RANKS.index(player.table[4].rank) + 51] = 1
-        temp[State.SUITS.index(player.table[4].suit) + 64] = 1
+    if pf0 and pf1:
+        temp[State.RANKS.index(pf0.rank)] = 1
+        temp[State.SUITS.index(pf0.suit) + 13] = 1
+        temp[State.RANKS.index(pf1.rank)] = 1
+        temp[State.SUITS.index(pf1.suit) + 13] = 1
+    if f0 and f1 and f2:
+        temp[State.RANKS.index(f0.rank) + 17] = 1
+        temp[State.SUITS.index(f0.suit) + 30] = 1
+        temp[State.RANKS.index(f1.rank) + 17] = 1
+        temp[State.SUITS.index(f1.suit) + 30] = 1
+        temp[State.RANKS.index(f2.rank) + 17] = 1
+        temp[State.SUITS.index(f2.suit) + 30] = 1
+    if t:
+        temp[State.RANKS.index(t.rank) + 34] = 1
+        temp[State.SUITS.index(t.suit) + 47] = 1
+    if r:
+        temp[State.RANKS.index(r.rank) + 51] = 1
+        temp[State.SUITS.index(r.suit) + 64] = 1
 
     return temp
 
 
 def get_array_inputs_cards_and_bot_actions(bot=None, call_limit=0, bet_limit=0):
+    return get_array_from_stages_for_cards_and_action_bot(
+        bot.hand[0], bot.hand[1], bot.table[0], bot.table[1], bot.table[2], bot.table[3], bot.table[4], call_limit,
+        bet_limit
+    )
+
+
+def get_array_for_cards_and_action_bot_from_map(m=None):
+    return get_array_from_stages_for_cards_and_action_bot(
+        m.get('pf0'), m.get('pf1'), m.get('f0'), m.get('f1'), m.get('f2'), m.get('t'), m.get('r')
+    )
+
+
+def get_array_from_stages_for_cards_and_action_bot(pf0, pf1, f0, f1, f2, t, r, call_limit=0, bet_limit=0):
     temp = [0] * 268
 
-    if len(bot.hand) > 1:
-        temp[State.RANKS.index(bot.hand[0].rank)] = 1
-        temp[State.SUITS.index(bot.hand[0].suit) + 13] = 1
-        temp[State.RANKS.index(bot.hand[1].rank)] = 1
-        temp[State.SUITS.index(bot.hand[1].suit) + 13] = 1
-    if len(bot.table) > 2:
-        temp[State.RANKS.index(bot.table[0].rank) + 17] = 1
-        temp[State.SUITS.index(bot.table[0].suit) + 30] = 1
-        temp[State.RANKS.index(bot.table[1].rank) + 17] = 1
-        temp[State.SUITS.index(bot.table[1].suit) + 30] = 1
-        temp[State.RANKS.index(bot.table[2].rank) + 17] = 1
-        temp[State.SUITS.index(bot.table[2].suit) + 30] = 1
-    if len(bot.table) > 3:
-        temp[State.RANKS.index(bot.table[3].rank) + 34] = 1
-        temp[State.SUITS.index(bot.table[3].suit) + 47] = 1
-    if len(bot.table) > 4:
-        temp[State.RANKS.index(bot.table[4].rank) + 51] = 1
-        temp[State.SUITS.index(bot.table[4].suit) + 64] = 1
+    if pf0 and pf1:
+        temp[State.RANKS.index(pf0.rank)] = 1
+        temp[State.SUITS.index(pf0.suit) + 13] = 1
+        temp[State.RANKS.index(pf1.rank)] = 1
+        temp[State.SUITS.index(pf1.suit) + 13] = 1
+    if f0 and f1 and f2:
+        temp[State.RANKS.index(f0.rank) + 17] = 1
+        temp[State.SUITS.index(f0.suit) + 30] = 1
+        temp[State.RANKS.index(f1.rank) + 17] = 1
+        temp[State.SUITS.index(f1.suit) + 30] = 1
+        temp[State.RANKS.index(f2.rank) + 17] = 1
+        temp[State.SUITS.index(f2.suit) + 30] = 1
+    if t:
+        temp[State.RANKS.index(t.rank) + 34] = 1
+        temp[State.SUITS.index(t.suit) + 47] = 1
+    if r:
+        temp[State.RANKS.index(r.rank) + 51] = 1
+        temp[State.SUITS.index(r.suit) + 64] = 1
     temp[68 + call_limit] = 1
     temp[167 + bet_limit] = 1
 
@@ -94,9 +120,9 @@ BOT_CUSTOM_ACTIONS = config.PLAYING_MODE and config.NN_INPUTS_CARDS
 BOT_NN_ACTIONS = config.PLAYING_MODE and config.NN_INPUTS_CARDS_AND_BOT_ACTIONS
 BOT_ALWAYS_SKIP = config.LEARNING_MODE and config.NN_INPUTS_CARDS
 
-if bool(config.LEARNING_MODE) + bool(config.PLAYING_MODE) + bool(config.PREDICTING_MODE) != 1:
-    print(colored("\t\t\t\t\t\tONLY ONE VARIABLE MAY BE EQUALS TRUE (LEARNING_MODE, PLAYING_MODE, PREDICTING_MODE)", "red"))
+if (bool(config.LEARNING_MODE) + bool(config.PLAYING_MODE) + bool(config.PREDICTING_MODE)) != 1:
+    print(colored("\t\tONLY ONE VARIABLE MAY BE EQUALS TRUE (LEARNING_MODE, PLAYING_MODE, PREDICTING_MODE)", "red"))
     exit(0)
-if bool(config.NN_INPUTS_CARDS_AND_BOT_ACTIONS) + bool(config.NN_INPUTS_CARDS) != 1:
-    print(colored("\t\t\t\t\t\tONLY ONE VARIABLE MAY BE EQUALS TRUE (NN_INPUTS_CARDS_AND_BOT_ACTIONS, NN_INPUTS_CARDS)", "red"))
+if (bool(config.NN_INPUTS_CARDS_AND_BOT_ACTIONS) + bool(config.NN_INPUTS_CARDS)) != 1:
+    print(colored("\t\tONLY ONE VARIABLE MAY BE EQUALS TRUE (NN_INPUTS_CARDS_AND_BOT_ACTIONS, NN_INPUTS_CARDS)", "red"))
     exit(0)
--- api/modes/predicting_mode.py
+++ api/modes/predicting_mode.py
@@ -1,25 +1,20 @@
 from api.helpers.model import Model
 from api.helpers.utils import get_array_inputs_cards
-from api.poker import core
-from api.poker.core import Core
+from api.poker.card import Card
 
 
-# todo
 class PredictingMode(object):
-    def __init__(self):
-        test = []
-        c_map0 = {
-            'pf0': core.Card(14, 'd'),
-            'pf1': core.Card(12, 's'),
-            'f0': core.Card(4, 'd'),
-            'f1': core.Card(7, 'c'),
-            'f2': core.Card(5, 'd'),
-            't': core.Card(10, 'd'),
-            'r': core.Card(9, 'd')
-        }
-
     @staticmethod
     def action():
-        results = Model.dnn.predict([get_array_inputs_cards(Core.players[0])])
+        c_map0 = {
+            'pf0': Card(14, 'd'),
+            'pf1': Card(12, 's'),
+            'f0': Card(4, 'd'),
+            'f1': Card(7, 'c'),
+            'f2': Card(5, 'd'),
+            't': Card(10, 'd'),
+            'r': Card(9, 'd')
+        }
+        results = Model.dnn.predict([get_array_inputs_cards(c_map0)])
         for r in results:
             print("PREDICT={:5f}\n".format(r[0]))
