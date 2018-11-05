b86556d2112bb737d6eb1d622192e3eb69e24ce8
emischuk <emishuk@mmtr.ru>
2018-11-05 09:41:23 +0200

    predicting mode

--- api/modes/predicting_mode.py
+++ api/modes/predicting_mode.py
@@ -6,8 +6,43 @@ from api.poker.card import Card
 
 
 class PredictingMode(object):
+
     @staticmethod
     def action():
+        def validate_string(c):
+            converted_map = {
+                'A': 14,
+                'K': 13,
+                'Q': 12,
+                'J': 11,
+                'T': 10,
+                '14': 14,
+                '13': 13,
+                '12': 12,
+                '11': 11,
+                '10': 10,
+                '9': 9,
+                '8': 8,
+                '7': 7,
+                '6': 6,
+                '5': 5,
+                '4': 4,
+                '3': 3,
+                '2': 2,
+                's': 's',
+                'h': 'h',
+                'd': 'd',
+                'c': 'c'
+            }
+            rank = converted_map.get(c.rank)
+            suit = converted_map.get(c.suit)
+            if rank and suit:
+                c.rank = rank
+                c.suit = suit
+                return c
+            else:
+                print_error("Invalid card format")
+
         pd = config.PREDICTING_DATA
         pd_len = pd.__len__()
         if pd and pd_len % 2 == 0:
@@ -16,13 +51,13 @@ class PredictingMode(object):
             else:
                 Model.init_tf_model_with_input_cards_and_bot_actions(load=True)
 
-            pf0 = Card(pd[0], pd[1]).validate_string() if pd_len > 1 else None
-            pf1 = Card(pd[2], pd[3]).validate_string() if pd_len > 3 else None
-            f0 = Card(pd[4], pd[5]).validate_string() if pd_len > 5 else None
-            f1 = Card(pd[6], pd[7]).validate_string() if pd_len > 7 else None
-            f2 = Card(pd[8], pd[9]).validate_string() if pd_len > 9 else None
-            t = Card(pd[10], pd[11]).validate_string() if pd_len > 11 else None
-            r = Card(pd[12], pd[13]).validate_string() if pd_len > 13 else None
+            pf0 = validate_string(Card(pd[0], pd[1])) if pd_len > 1 else None
+            pf1 = validate_string(Card(pd[2], pd[3])) if pd_len > 3 else None
+            f0 = validate_string(Card(pd[4], pd[5])) if pd_len > 5 else None
+            f1 = validate_string(Card(pd[6], pd[7])) if pd_len > 7 else None
+            f2 = validate_string(Card(pd[8], pd[9])) if pd_len > 9 else None
+            t = validate_string(Card(pd[10], pd[11])) if pd_len > 11 else None
+            r = validate_string(Card(pd[12], pd[13])) if pd_len > 13 else None
 
             if config.NN_INPUTS_CARDS:
                 results = Model.dnn.predict([get_array_from_stages_for_cards(pf0, pf1, f0, f1, f2, t, r)])
--- api/poker/card.py
+++ api/poker/card.py
@@ -1,6 +1,3 @@
-from api.helpers.message import print_error
-
-
 class Card(object):
     def __init__(self, rank, suit):
         self.rank = rank
@@ -38,52 +35,6 @@ class Card(object):
         if not rank:
             rank = self.rank
         return str(rank)
-
-    def validate_string(self):
-        converted_map = {
-            'A': 14,
-            'K': 13,
-            'Q': 12,
-            'J': 11,
-            'T': 10,
-            '14': 14,
-            '13': 13,
-            '12': 12,
-            '11': 11,
-            '10': 10,
-            '9': 9,
-            '8': 8,
-            '7': 7,
-            '6': 6,
-            '5': 5,
-            '4': 4,
-            '3': 3,
-            '2': 2,
-            's': 's',
-            'h': 'h',
-            'd': 'd',
-            'c': 'c'
-        }
-        rank = converted_map.get(self.rank)
-        suit = converted_map.get(self.suit)
-        if rank and suit:
-            self.rank = rank
-            self.suit = suit
-        else:
-            print_error("Invalid format card")
-        return self
-
-    def suit_str(self):
-        converted_map = {
-            's': u"\u2660",
-            'h': u"\u2764",
-            'd': u"\u2666",
-            'c': u"\u2663"
-        }
-        suit = converted_map.get(self.suit)
-        if not suit:
-            suit = self.suit
-        return str(suit)
 
     def __eq__(self, other):
         return self.rank == other.rank
